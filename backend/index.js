e3d07c6aca0b5cab5afee9da83d6781f1cebce36
moh-ammad <mohammadsohail9392@gmail.com>
2025-10-08 19:30:02 +0530

    solved cors issue

--- backend/index.js
+++ backend/index.js
@@ -23,7 +23,6 @@ const corsOptions = {
   },
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
   optionsSuccessStatus: 200
 };
 
